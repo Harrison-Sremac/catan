4e5e3c949b8c0ec4773c6cca6c881dddffced3e0
Harrison Laptop <hcs59@cornell.edu>
2024-05-17 18:06:57 -0400

    added some files

--- gen_board.py
+++ gen_board.py
@@ -1,1 +1,9 @@
 import random
+
+def gen_board():
+  # This function will print a random catan board this is useful
+  # for creating the intial set up and for quickly making games
+  # numbers will correlate to different biomes
+  # there should be 19 tiles in Catan generated
+  return "h"
+print(gen_board)
