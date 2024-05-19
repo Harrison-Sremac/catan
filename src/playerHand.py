19dab8725e9d6cb1f25fab3d36bf9616d90b7cea
Tony <tkseagull@protonmail.com>
2024-05-19 12:35:12 -0400

    added playerHand class boiler plate code

---
+++ src/playerHand.py
@@ -0,0 +1,25 @@
+"""
+This class wil handle the cards that the players have in their hands
+"""
+class playerHand:
+    
+    def __init__(self):
+
+        playerCards = []
+        cardCount = playerCards.length()
+        
+
+    def tradeCard():
+        pass
+    
+    def removeCard():
+        pass
+
+    def addCard():
+        pass
+    
+    def robCard(): #This method is used when a robber takes a card from another player
+        pass
+    
+    def dsicardHalf(): #This method is used for when a seven is rolled and any player with more than seven cards must discard half his hand
+        pass
