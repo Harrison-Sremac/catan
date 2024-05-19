9506dfccbf44ae2c5dc5fc63eb6936684475843b
Harrison Laptop <hcs59@cornell.edu>
2024-05-18 20:04:20 -0400

    updates

--- src/board.py
+++ src/board.py
@@ -21,3 +21,11 @@ class Board:
         self.nodes.append(Node("ore"))
         # add edges
         return 0
+    
+    def gen_board():
+    # This function will print a random catan board this is useful
+    # for creating the intial set up and for quickly making games
+    # numbers will correlate to different biomes
+    # there should be 19 tiles in Catan generated
+        biomes = ['Forest', 'Pasture', 'Fields', 'Hills', 'Mountains', 'Desert']
+  
--- src/node.py
+++ src/node.py
@@ -5,7 +5,44 @@ class Node:
 
     def __init__(self, r):
         self.resource = r
-        if self.resource == None:
-            self.city = None
+        self.next = None
+        self.owner = None
+        self.building_type = None  # 'settlement', 'city', or None
+        self.adjacent_resources = []
+    
+    """
+    Methods meant for adding or removing resource cards from players hands
+    """
+    def insertAtEnd(self, r):
+        newNode = Node(r)
+        if self.head is None:
+            self.head = newNode
+            return
+
+        currentNode = self.head
+        while(currentNode.next):
+            currentNode = currentNode.next
+
+        currentNode.next = newNode
+
+    def removeFirstNode(self):
+        if(self.head == None):
+            return
+        
+        self.head = self.head.next
+
+    def deleteResource(self, r):
+        currentNode = self.head
+        if currentNode.r == r:
+            self.removeFirstNode()
+            return
+        while currentNode is not None and currentNode.next.data != r:
+            currentNode = currentNode.next
+ 
+        if currentNode is None:
+            return
         else:
-            self.is_robber = False
+            currentNode.next = currentNode.next.next
+            
+            
+        
