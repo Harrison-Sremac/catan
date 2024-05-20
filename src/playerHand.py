5ae6eaee661d16b43b162500b2ace78b4eea1d8b
Tony <tkseagull@protonmail.com>
2024-05-20 10:22:33 -0400

    Implemented some of the methods in playerHand

--- src/playerHand.py
+++ src/playerHand.py
@@ -4,21 +4,21 @@ This class wil handle the cards that the players have in their hands
 class playerHand:
     
     def __init__(self):
+        self.playerCards = []
+        self.cardCount = len(self.playerCards)
 
-        playerCards = []
-        cardCount = playerCards.length()
-        
-
-    def tradeCard():
+    def tradeCard(self):
         pass
+
+    def removeCard(self, resource):
+        if resource in self.playerCards:
+            self.playerCards.remove(resource)
+            self.cardCount = len(self.playerCards)
+
+    def addCard(self, resource):
+        self.playerCards.append(resource)
     
-    def removeCard():
-        pass
-
-    def addCard():
-        pass
-    
-    def robCard(): #This method is used when a robber takes a card from another player
+    def robCard(self, color, resource): #This method is used when a robber takes a card from another player. Color represents another player.
         pass
     
     def dsicardHalf(): #This method is used for when a seven is rolled and any player with more than seven cards must discard half his hand
